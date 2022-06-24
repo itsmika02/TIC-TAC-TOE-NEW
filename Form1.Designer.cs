c5933c50808c034e04c8f4b03145062ee28a669d
Michaela Espiritu <104263583+itsmika02@users.noreply.github.com>
2022-06-24 23:55:59 +0800

    Add files via upload
    New Game, Exit, and 9 Buttons are finally functioning!

---
+++ Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace TIC_TAC_TOE_GAME
+{
+    partial class TICTACTOE
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TICTACTOE));
+            this.NewGameButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button5 = new System.Windows.Forms.Button();
+            this.Button6 = new System.Windows.Forms.Button();
+            this.Button7 = new System.Windows.Forms.Button();
+            this.Button8 = new System.Windows.Forms.Button();
+            this.Button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NewGameButton
+            // 
+            this.NewGameButton.BackColor = System.Drawing.Color.SaddleBrown;
+            this.NewGameButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("NewGameButton.BackgroundImage")));
+            this.NewGameButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.NewGameButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.NewGameButton.ForeColor = System.Drawing.Color.Black;
+            this.NewGameButton.Location = new System.Drawing.Point(26, 23);
+            this.NewGameButton.Name = "NewGameButton";
+            this.NewGameButton.Size = new System.Drawing.Size(126, 34);
+            this.NewGameButton.TabIndex = 0;
+            this.NewGameButton.Text = "NEW GAME";
+            this.NewGameButton.UseVisualStyleBackColor = false;
+            this.NewGameButton.Click += new System.EventHandler(this.NewGameButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.BackColor = System.Drawing.Color.SaddleBrown;
+            this.ExitButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("ExitButton.BackgroundImage")));
+            this.ExitButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.ExitButton.ForeColor = System.Drawing.Color.Black;
+            this.ExitButton.Location = new System.Drawing.Point(323, 23);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(126, 34);
+            this.ExitButton.TabIndex = 1;
+            this.ExitButton.Text = "EXIT";
+            this.ExitButton.UseVisualStyleBackColor = false;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // Button1
+            // 
+            this.Button1.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button1.BackgroundImage")));
+            this.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button1.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button1.ForeColor = System.Drawing.Color.Wheat;
+            this.Button1.Location = new System.Drawing.Point(98, 124);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(85, 85);
+            this.Button1.TabIndex = 2;
+            this.Button1.UseVisualStyleBackColor = false;
+            this.Button1.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button2
+            // 
+            this.Button2.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button2.BackgroundImage")));
+            this.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button2.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button2.ForeColor = System.Drawing.Color.Wheat;
+            this.Button2.Location = new System.Drawing.Point(189, 124);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(85, 85);
+            this.Button2.TabIndex = 3;
+            this.Button2.UseVisualStyleBackColor = false;
+            this.Button2.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button3
+            // 
+            this.Button3.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button3.BackgroundImage")));
+            this.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button3.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button3.ForeColor = System.Drawing.Color.Wheat;
+            this.Button3.Location = new System.Drawing.Point(280, 124);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(85, 85);
+            this.Button3.TabIndex = 4;
+            this.Button3.UseVisualStyleBackColor = false;
+            this.Button3.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button4
+            // 
+            this.Button4.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button4.BackgroundImage")));
+            this.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button4.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button4.ForeColor = System.Drawing.Color.Wheat;
+            this.Button4.Location = new System.Drawing.Point(98, 215);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(85, 85);
+            this.Button4.TabIndex = 5;
+            this.Button4.UseVisualStyleBackColor = false;
+            this.Button4.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button5
+            // 
+            this.Button5.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button5.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button5.BackgroundImage")));
+            this.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button5.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button5.ForeColor = System.Drawing.Color.Wheat;
+            this.Button5.Location = new System.Drawing.Point(189, 215);
+            this.Button5.Name = "Button5";
+            this.Button5.Size = new System.Drawing.Size(85, 85);
+            this.Button5.TabIndex = 6;
+            this.Button5.UseVisualStyleBackColor = false;
+            this.Button5.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button6
+            // 
+            this.Button6.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button6.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button6.BackgroundImage")));
+            this.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button6.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button6.ForeColor = System.Drawing.Color.Wheat;
+            this.Button6.Location = new System.Drawing.Point(280, 215);
+            this.Button6.Name = "Button6";
+            this.Button6.Size = new System.Drawing.Size(85, 85);
+            this.Button6.TabIndex = 7;
+            this.Button6.UseVisualStyleBackColor = false;
+            this.Button6.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button7
+            // 
+            this.Button7.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button7.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button7.BackgroundImage")));
+            this.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button7.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button7.ForeColor = System.Drawing.Color.Wheat;
+            this.Button7.Location = new System.Drawing.Point(98, 306);
+            this.Button7.Name = "Button7";
+            this.Button7.Size = new System.Drawing.Size(85, 85);
+            this.Button7.TabIndex = 8;
+            this.Button7.UseVisualStyleBackColor = false;
+            this.Button7.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button8
+            // 
+            this.Button8.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button8.BackgroundImage")));
+            this.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button8.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button8.ForeColor = System.Drawing.Color.Wheat;
+            this.Button8.Location = new System.Drawing.Point(189, 306);
+            this.Button8.Name = "Button8";
+            this.Button8.Size = new System.Drawing.Size(85, 85);
+            this.Button8.TabIndex = 9;
+            this.Button8.UseVisualStyleBackColor = false;
+            this.Button8.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // Button9
+            // 
+            this.Button9.BackColor = System.Drawing.Color.SaddleBrown;
+            this.Button9.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Button9.BackgroundImage")));
+            this.Button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Button9.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Button9.ForeColor = System.Drawing.Color.Wheat;
+            this.Button9.Location = new System.Drawing.Point(280, 306);
+            this.Button9.Name = "Button9";
+            this.Button9.Size = new System.Drawing.Size(85, 85);
+            this.Button9.TabIndex = 10;
+            this.Button9.UseVisualStyleBackColor = false;
+            this.Button9.Click += new System.EventHandler(this.Button_Click);
+            // 
+            // TICTACTOE
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(474, 426);
+            this.Controls.Add(this.Button9);
+            this.Controls.Add(this.Button8);
+            this.Controls.Add(this.Button7);
+            this.Controls.Add(this.Button6);
+            this.Controls.Add(this.Button5);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.Button2);
+            this.Controls.Add(this.Button1);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.NewGameButton);
+            this.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.Moccasin;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "TICTACTOE";
+            this.Text = "TIC TAC TOE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewGameButton;
+        private System.Windows.Forms.Button ExitButton;
+        private System.Windows.Forms.Button Button1;
+        private System.Windows.Forms.Button Button2;
+        private System.Windows.Forms.Button Button3;
+        private System.Windows.Forms.Button Button4;
+        private System.Windows.Forms.Button Button5;
+        private System.Windows.Forms.Button Button6;
+        private System.Windows.Forms.Button Button7;
+        private System.Windows.Forms.Button Button8;
+        private System.Windows.Forms.Button Button9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TIC_TAC_TOE_GAME
+{
+    public partial class TICTACTOE : Form
+    {
+        public TICTACTOE()
+        {
+            InitializeComponent();
+        }
+
+        public int player = 2; // even = X turn; odd = O turn;
+        public int turns; // counting turns;
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button B = (Button)sender;
+            if (B.Text == "")
+            {
+                if (player % 2 == 0)
+                {
+                    B.Text = "X";
+                    player++;
+                    turns++;
+                }
+                else
+                {
+                    B.Text = "O";
+                    player++;
+                    turns++;
+                }
+
+      
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        void NewGame()
+        {
+            player = 2;
+            turns = 0;
+            Button1.Text = Button2.Text = Button3.Text = Button4.Text = Button5.Text = Button6.Text = Button7.Text = Button8.Text = Button9.Text = "";
+
+        }
+
+        private void NewGameButton_Click(object sender, EventArgs e)
+        {
+            NewGame();
+        }
+    }
+}
+
+
+
+
